1526e0fff5338415f1d2fd0dc0d7b7da042b41d9
Changbeom Ahn <kewlbear@gmail.com>
2021-10-13 11:16:50 +0900

    update Python sources to 0.13

--- Sources/Open3DSupport/site-packages/open3d/core.py
+++ Sources/Open3DSupport/site-packages/open3d/core.py
@@ -27,12 +27,14 @@
 import open3d as o3d
 
 if o3d.__DEVICE_API__ == 'cuda':
-    from open3d.cuda.pybind.core import (Tensor, Hashmap, Dtype, DtypeCode,
-                                         Device, cuda, nns, NoneType,
+    from open3d.cuda.pybind.core import (Tensor, Scalar, Hashmap, Dtype,
+                                         DtypeCode, Device, cuda, nns,
                                          SizeVector, DynamicSizeVector, matmul,
-                                         lstsq, solve, inv, svd)
+                                         lstsq, solve, det, lu, lu_ipiv, inv,
+                                         svd, triu, tril, triul)
 else:
-    from open3d.cpu.pybind.core import (Tensor, Hashmap, Dtype, DtypeCode,
-                                        Device, cuda, nns, NoneType, SizeVector,
-                                        DynamicSizeVector, matmul, lstsq, solve,
-                                        inv, svd)
+    from open3d.cpu.pybind.core import (Tensor, Scalar, Hashmap, Dtype,
+                                        DtypeCode, Device, cuda, nns,
+                                        SizeVector, DynamicSizeVector, matmul,
+                                        lstsq, solve, det, lu, lu_ipiv, inv,
+                                        svd, triu, tril, triul)
