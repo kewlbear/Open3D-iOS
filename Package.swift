9cfc3e36b70d9fc5072fc8963fd3974c0d2a6490
MAGRAF Studio <contact@magraf.fr>
2022-10-19 11:22:30 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,9 +4,6 @@ import PackageDescription
 
 let package = Package(
     name: "Open3D-iOS",
-    platforms: [
-        .iOS("14")
-    ],
     products: [
         .library(
             name: "Open3D-iOS",
